a3107c4b53b52ad221837a78d5c7c4834a16c5b7
Saurabh Shukla <84230669+Sam-Tennyson@users.noreply.github.com>
2023-04-01 21:20:38 +0530

    new change

--- services/cause_services.js
+++ services/cause_services.js
@@ -8,6 +8,13 @@ let causeService = {}
 causeService.create = async (payload) => await new CauseSchema(payload).save();
 
 /**
+* function to update one.
+*/
+causeService.findOneAndUpdate = async (criteria, dataToUpdate, projection = {}) => await CauseSchema
+  .findOneAndUpdate(criteria, dataToUpdate, projection).lean();
+
+
+/**
 * function to find one.
 */
 causeService.findOne = async (criteria, projection = {}) => await CauseSchema.findOne(criteria, projection).lean();
